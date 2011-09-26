713feab2bcb6049278d3f7ae66e547025c31217d
Alex Shulgin <ash@commandprompt.com>
2011-09-26 12:42:42 +0300

    Renamed view hook partials to unique names.

--- lib/redmine_canned_responses/view_hooks.rb
+++ lib/redmine_canned_responses/view_hooks.rb
@@ -1,9 +1,9 @@
 module RedmineCannedResponses
   class ViewHooks < Redmine::Hook::ViewListener
     render_on :view_layouts_base_html_head,
-      :partial => 'hooks/view_layouts_base_html_head'
+      :partial => 'hooks/canned_responses_stylesheet'
 
     render_on :view_issues_edit_notes_bottom,
-      :partial => 'hooks/view_issues_edit_notes_bottom'
+      :partial => 'hooks/canned_responses_issue_insert'
   end
 end
