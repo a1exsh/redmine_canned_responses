6a32380371617ad6a98f282b8143e1c8bfe865d8
Alex Shulgin <ash@commandprompt.com>
2012-07-23 06:03:25 -0700

    Fix preview/insert routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :canned_responses,
-    :member => { :preview => :any, :insert => :post }
+    :collection => { :preview => :post, :insert => :post }
   map.resources :canned_responses, :path_prefix => '/projects/:project_id',
-    :member => { :preview => :any, :insert => :post }
+    :collection => { :preview => :post, :insert => :post }
 end
